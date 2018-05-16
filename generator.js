8fdc05994b37f22925730a58eeedb38f440ef1d3
ricksmith26 <ricksmith26@hotmail.com>
2018-05-16 17:18:05 +0100

    generator with notes

--- generator.js
+++ generator.js
@@ -1,25 +1,26 @@
 var fs = require('fs');
 
+//1st mkdir is the main folder for project, every other file for the project must be nested inside
 fs.mkdir('newProject', function(err) {
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
-
-  fs.writeFile('index.js', '', function(err) {
+  //index file nested in main mkdir
+  fs.writeFile('newProject/index.js', '', function(err) {
     if (err) {
       return console.log(err);
     }
     console.log('new index.js file created successfully');
   });
-
-  fs.mkdir('spec', function(err) {
+  //folder for spec is created inside main folder
+  fs.mkdir('newProject/spec', function(err) {
     if (err) {
       return console.error(err);
     }
     console.log('Spec Directory created successfully!');
-
-    fs.writeFile('index.spec.js', '', function(err) {
+    // new spec file is then created inside the spec folder
+    fs.writeFile('newProject/spec/index.spec.js', '', function(err) {
       if (err) {
         return console.log(err);
       }
